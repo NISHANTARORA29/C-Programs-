902bf472b64e48cceb379e1f5aa1a860228ac33e
Lokesh monani <92055680+MonaniLokesh@users.noreply.github.com>
2024-03-10 23:07:37 +0530

    Update Check_whether_a_Triangle_is_Equilateral_Isosceles_or_Scalene.c

--- Check_whether_a_Triangle_is_Equilateral_Isosceles_or_Scalene.c
+++ Check_whether_a_Triangle_is_Equilateral_Isosceles_or_Scalene.c
@@ -1,9 +1,9 @@
-#include < stdio.h >  
-int main()  
-{  
-    float a, b, c, flag = 0;  
-    printf("Enter values for a, b and c\n");  
-    scanf("%f%f%f", &a, &b, &c);  
+// #include < stdio.h >  
+// int main()  
+// {  
+//     float a, b, c, flag = 0;  
+//     printf("Enter values for a, b and c\n");  
+//     scanf("%f%f%f", &a, &b, &c);  
         // if(a == b && b == c){  
         //     printf("It's an Equilateral Triangle\n");  
         // }  
@@ -13,5 +13,5 @@ int main()
         // else{  
         //     printf("It's a Scalene Triangle\n");  
         // }  
-    return 0;  
-}  
+    // return 0;  
+// }  
